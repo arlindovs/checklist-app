ab574e1076fd8fb834935bd40937e98233285820
Arlindo Silva <arlindovicente@gmail.com>
2023-10-12 20:39:17 -0300

    tabela

--- src/app/category/category.component.ts
+++ src/app/category/category.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CATEGORY_DATA, Category } from '../_models/category';
 
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
   styleUrls: ['./category.component.css']
 })
-export class CategoryComponent {
+export class CategoryComponent implements OnInit {
+
+  public displayedColumns: string[] = ['id', 'name', 'actions'];
+  public dataSource: Category[] = CATEGORY_DATA;
+
+  constructor(){ }
+
+  ngOnInit(): void {
+  }
+
+  public editCategory(category: Category){
+    console.log('Cliquei em Editar!');
+  }
+
+  public deleteCategory(category: Category){
+    console.log('Cliquei em Deletar!');
+  }
+
+  public createNewCategory(){
+    console.log('Cliquei em Novo!');
+  }
 
 }
