45d715c387d174efafe5246565455ccf39c22ec5
Arlindo Silva <arlindovicente@gmail.com>
2023-10-13 16:40:10 -0300

    organizacao de codigo

--- src/app/category/category.component.ts
+++ src/app/category/category.component.ts
@@ -6,38 +6,41 @@ import { DialogComponent } from '../dialog/dialog.component';
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
-  styleUrls: ['./category.component.css']
+  styleUrls: ['./category.component.css'],
 })
 export class CategoryComponent implements OnInit {
-
   public displayedColumns: string[] = ['id', 'name', 'actions'];
   public dataSource: Category[] = CATEGORY_DATA;
 
-  constructor(private dialog: MatDialog){ }
+  constructor(private dialog: MatDialog) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  public editCategory(category: Category){
+  public editCategory(category: Category) {
     console.log('Cliquei em Editar!');
   }
 
-  public deleteCategory(category: Category){
-    this.dialog.open(DialogComponent, {disableClose: true,
-      data: {dialogMsg: 'Realmente deseja apagar a categoria?',
-            leftButtonLabel: 'Não', rightButtonLabel: 'Sim'}}).afterClosed().subscribe(
-      resp => {
-        if(resp){
-          console.log("Apagado com Sucesso!")
+  public deleteCategory(category: Category) {
+    this.dialog
+      .open(DialogComponent, {
+        disableClose: true,
+        data: {
+          dialogMsg: 'Realmente deseja apagar a categoria?',
+          leftButtonLabel: 'Não',
+          rightButtonLabel: 'Sim',
+        },
+      })
+      .afterClosed()
+      .subscribe((resp) => {
+        if (resp) {
+          console.log('Apagado com Sucesso!');
         } else {
-          console.log("Erro ao Apagar!")
+          console.log('Erro ao Apagar!');
         }
-      }
-    )
+      });
   }
 
-  public createNewCategory(){
+  public createNewCategory() {
     console.log('Cliquei em Novo!');
   }
-
 }
